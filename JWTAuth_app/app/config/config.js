c487edbd56637d943fa3b023469f0adb1dfe3518
mkhachane <mayur.khachane@opcito.com>
2018-12-27 15:59:22 +0530

    Add some comments

--- JWTAuth_app/app/config/config.js
+++ JWTAuth_app/app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
       'secret' : 'grokonez-super-secret-key',
-      url : 'mongodb://<username>:<password>@ds061506.mlab.com:61506/products',
+      url : 'mongodb://<username>:<password>@ds061506.mlab.com:61506/<dbname>',
       ROLEs :['USER','ADMIN', 'SUPERUSER']
 };
